ec89a511432a7738f7b0afb427c69ccc8df17e67
Fran Aguilera (MODERNAPPS) <fraguile@microsoft.com>
2019-12-05 12:02:07 -0500

    Made it readable

--- Format-Refactor.psm1
+++ Format-Refactor.psm1
@@ -18,7 +18,14 @@
         | Where-Object { ! $_.PSIsContainer } `
         | ForEach-Object `
         {
-            (Get-Content -Path $_.PSPath -Raw) -replace '([ \t]*)(if[ \t]*\(.*\))[ \t]*(\r\n|\r|\n)*([ \t]*)([^{]*;)', "`$1`$2`$3`$1{`$3`$4`$5`$3`$1}" `
+            $regex = '([ \t]*)(if[ \t]*\(.*\))[ \t]*(\r\n|\r|\n)*([ \t]*)([^{]*;)'
+            $spaceBeforeIf = '`$1' # This is the spacing before the if statement
+            $ifStatement = '`$2' # This is the if statement itself
+            $newLine = '`$3' # This is the file-specific newline type being used
+            $spaceBeforeStatement = '`$4' # This is the spacing before the statement under the if
+            $statement = '`$5' # This is the statement under the if
+            $regexOutput = "$spaceBeforeIf$ifStatement$newLine$spaceBeforeIf{$newLine$spaceBeforeStatement$statement$newLine$spaceBeforeIf}"
+            (Get-Content -Path $_.PSPath -Raw) -replace $regex, $regexOutput `
             | Out-File $_.PSPath
         }
 }
