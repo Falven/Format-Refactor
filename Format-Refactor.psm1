6b129ad5c27078550cdf0eace1d6aa35a55d03d5
Fran Aguilera <fraguile@microsoft.com>
2019-12-11 10:05:10 -0500

    Removed DependencyInjectionChecks function.

--- Format-Refactor.psm1
+++ Format-Refactor.psm1
@@ -13,6 +13,7 @@
     )
 
     # Outline changes to user before proceeding
+    # Support /*comment*/
 
     Get-ChildItem -Path $Path -Recurse `
         | Where-Object { ! $_.PSIsContainer } `
@@ -28,29 +29,4 @@
             (Get-Content -Path $_.PSPath -Raw) -replace $regex, $regexOutput `
             | Out-File $_.PSPath
         }
-}
-
-function Format-DependencyInjectionChecks {
-    param(
-        [ValidateScript({
-            if($null -eq $_) {
-                throw "You must provide a valid path."
-            }
-            if(-Not ($_ | Test-Path) ) {
-                throw "File or folder does not exist."
-            }
-            return $true
-        })]
-        [System.IO.FileInfo]$Path
-    )
-
-    # Existing if null checks or Null coalescing
-
-    Get-ChildItem -Path $Path -Recurse | Where-Object { ! $_.PSIsContainer } `
-        | ForEach-Object `
-        {
-            (Get-Content -Path $_.PSPath -Raw) -replace '([ \t]*)(if[ \t]*\(.*\))[ \t]*(\r\n|\r|\n)*([ \t]*)([^{]*;)', "`$1`$2`$3`$1{`$3`$4`$5`$3`$1}" `
-            | Out-File $_.PSPath
-        }
-    Write-Output 'Finished refactor.'
 }
